6243a1604a4d0595108e929e1b8663190ff1ccbf
Arunsh4h <98868947+Arunsh4h@users.noreply.github.com>
2023-04-15 00:22:21 +0530

    tesst

--- wp-content/themes/lumenity/archive.php
+++
@@ -1,6 +0,0 @@
-<?php get_header();
-	$blog_style = askme_options("home_display");
-	$vbegy_sidebar_all = askme_options('sidebar_layout');
-	get_template_part("loop","archive");
-	vpanel_pagination();
-get_footer();?>
